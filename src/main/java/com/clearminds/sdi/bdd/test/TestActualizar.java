d3847f3b82280cc0679df205524bdb622198bb43
Sofía Díaz <soufito21@gmail.com>
2020-12-09 11:26:05 -0500

    Fuente método actualizar y TestActualizar

---
+++ src/main/java/com/clearminds/sdi/bdd/test/TestActualizar.java
@@ -0,0 +1,20 @@
+package com.clearminds.sdi.bdd.test;
+
+import com.clearminds.sdi.dtos.Estudiante;
+import com.clearminds.sdi.excepciones.BDDException;
+import com.clearminds.sdi.servicios.ServicioEstudiante;
+
+public class TestActualizar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ServicioEstudiante srvest = new ServicioEstudiante();
+		try {
+			srvest.actualizarEstudiante(new Estudiante("Pedro", "Navaja", 2));;
+		} catch (BDDException e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+	}
+
+}
--- src/main/java/com/clearminds/sdi/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/sdi/servicios/ServicioEstudiante.java
@@ -24,4 +24,22 @@ public class ServicioEstudiante extends ServicioBase {
 			throw new BDDException("Error al insertar estudiante");
 		}
 	}
+	
+	public void actualizarEstudiante(Estudiante est) throws BDDException{
+		Statement stm = null;
+
+		abrirConexion();
+		System.out.println("Actualizando estudiante: " + est);
+
+		try {
+			stm = getConexion().createStatement();
+			String sql = "update estudiantes set nombre='"+est.getNombre()+"', apellido='"+est.getApellido()+"' where id="+est.getId();
+			System.out.println("Script: " + sql);
+			stm.executeUpdate(sql);
+			cerrarConexion();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new BDDException("Error al actualizar estudiante");
+		}
+	}
 }
